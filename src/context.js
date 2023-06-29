bc87bc56e6bff502d04c9d2bb3904525d78777a3
meanev56 <emekanjoku86@gmail.com>
2023-06-29 17:17:40 +0100

    contex

--- src/context.js
+++ src/context.js
@@ -5,7 +5,7 @@ const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
     const [searchTerm, setSearchTerm] = useState("the lost world");
-    const [b00ks, setBooks] = useState([]);
+    const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(true);
     const [resultTitle, setResultTitle] = useState("");
 
@@ -16,6 +16,55 @@ const AppProvider = ({children}) => {
             const data = await response.json();
             console.log(data);
             const {docs} = data;
+            console.log(docs)
+
+            if(docs){
+                const newBooks = docs.slice(0, 20).map((bookSingle) => {
+                        const {key, author_name, cover_i, edition_count, first_publish_year, title} = bookSingle;
+
+                        return {
+                            id: key,
+                            author: author_name,
+                            cover_id: cover_i,
+                            edition_count: edition_count,
+                            first_publish_year: first_publish_year,
+                            title: title
+                        }
+                    }
+                );
+                setBooks(newBooks);
+
+                if(newBooks.length > 1){
+                    setResultTitle("Your Search Result");
+                } else {
+                    setResultTitle("No Search Result Found")
+                }
+            } else {
+                setBooks([])
+                    setResultTitle("No Search Result Found!");
+            }
+            setLoading(false)
+        } catch(error){
+            console.log(error);
+            setLoading(false);
         }
-    })
+     }, [searchTerm]);
+
+     useEffect(() => {
+        fetchBooks();
+     }, [searchTerm, fetchBooks]);
+
+     return (
+        <AppContext.Provider value={{
+            loading, books, setSearchTerm, resultTitle, setResultTitle
+        }}>
+            {children}
+        </AppContext.Provider>
+     )
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
 }
+
+export {AppContext, AppProvider};
