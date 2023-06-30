38ee399a7a2b6c84a5240e7f043f06ebc42a115a
meanev56 <emekanjoku86@gmail.com>
2023-06-30 14:18:30 +0100

    index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
 import { AppProvider } from './context';
 import './index.css';
-import Home from "./pages/Home/Home";
+import Home from './pages/Home/Home';
 import About from "./pages/About/About";
 import BookList from "./components/BookList/BookList";
 import BookDetails from "./components/BookDetails/BookDetails";
@@ -13,14 +15,13 @@ root.render(
   <AppProvider>
     <BrowserRouter>
       <Routes>
-       <Route path = "/" element = {<Home />}>
+        <Route path = "/" element = {<Home />}>
           <Route path = "about" element = {<About />} />
           <Route path = "book" element = {<BookList />} />
-          <Route path = "book/:id" element = {<BookDetails />} />
-
-       </Route>
+          <Route path = "/book/:id" element = {<BookDetails />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </AppProvider>
-
 );
+
