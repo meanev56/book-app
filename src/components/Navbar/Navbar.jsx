ea41cc0885ce2322eb71d0695453dfe01063f92e
meanev56 <emekanjoku86@gmail.com>
2023-06-30 16:50:27 +0100

    index

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import "./Navbar.css";
 import logoImg from "../../images/logo.png";
 import {HiOutlineMenuAlt3} from "react-icons/hi";
@@ -9,30 +9,27 @@ const Navbar = () => {
   const handleNavbar = () => setToggleMenu(!toggleMenu);
 
   return (
-    <nav className='navbar' id='navbar'>
+    <nav className='navbar' id = "navbar">
       <div className='container navbar-content flex'>
         <div className='brand-and-toggler flex flex-sb'>
-          <Link to="/" className='navbar-brand flex'>
-            <img src={logoImg} alt='site logo' />
+          <Link to = "/" className='navbar-brand flex'>
+            <img src = {logoImg} alt = "site logo" />
             <span className='text-uppercase fw-7 fs-24 ls-1'>bookhub</span>
           </Link>
-          <button type='button' className='navbar-toggler-btn' onClick={handleNavbar}>
-            <HiOutlineMenuAlt3 size={35} style={{
+          <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
+            <HiOutlineMenuAlt3 size = {35} style = {{
               color: `${toggleMenu ? "#fff" : "#010101"}`
             }} />
           </button>
         </div>
-        <div className={toggleMenu ? "navbar-collapse show-navbar-collapse" : "navbar-collpase"}>
-          <ul className='navbar-nav'>
-            <li className='nav-icon'>
-              <Link to = "book" className='nav-link text-uppercase fs-22 fw-6 ls-1'>
-                Home
-              </Link>
+
+        <div className={toggleMenu ? "navbar-collapse show-navbar-collapse" : "navbar-collapse"}>
+          <ul className = "navbar-nav">
+            <li className='nav-item'>
+              <Link to = "book" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>Home</Link>
             </li>
-            <li className='nav-icon'>
-              <Link to = "about" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>
-                about
-              </Link>
+            <li className='nav-item'>
+              <Link to = "about" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>about</Link>
             </li>
           </ul>
         </div>
@@ -41,4 +38,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar;
+export default Navbar
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import Header from "../../components/Header/Header"
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Header from '../../components/Header/Header';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
     <main>
-      <Header />
+        <Header />
+        <Outlet />
     </main>
   )
 }
